b3859faa27f2bb052160f2601cb78cb6ce44306c
Forrest Marvin <fmarvin1@binghamton.edu>
2011-04-16 21:30:07 -0400

    Documentation and code standards review for Model.java

--- src/Model.java
+++ src/Model.java
@@ -1,11 +1,28 @@
+/*
+ * Model.java:
+ * This file contains the model portion of the model/view/controller design.
+ */
 import java.util.ArrayList;
 
-public class Model 
-{
-	public ArrayList<Member> lookupMember(String firstName, 
-			String lastName) throws Exception
-	{
-		//throw new Exception();
+/*
+ * This class provides the model portion of the MVC framework.
+ */
+public class Model {
+	/*
+	 * This method provides functionality for looking up members based on name.
+	 * It services requests from the view. It does this by calling the
+	 * lookupMember method in the database abstraction layer and passing this
+	 * return value its own return value.
+	 * 
+	 * @param firstName First name of the member being looked up.
+	 * 
+	 * @param lastName Last name of the member being looked up.
+	 * 
+	 * @return An ArrayList of members matching the first name and last name
+	 * given.
+	 */
+	public ArrayList<Member> lookupMember(String firstName, String lastName)
+			throws Exception {
 		return DatabaseAbstraction.lookupMember(firstName, lastName);
 	}
 }
