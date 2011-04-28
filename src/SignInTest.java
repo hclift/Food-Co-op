69d89d3de19c64c4eeee081ebdcef6fa1536f522
khannon1 <khannon1@binghamton.edu>
2011-04-28 13:52:35 -0400

    Working on tests for signIntoKitchen in Model

--- src/SignInTest.java
+++ src/SignInTest.java
@@ -1,8 +1,11 @@
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.Date;
 
 import org.junit.Before;
+import org.junit.Test;
 
 
 public class SignInTest {
@@ -18,6 +21,22 @@ public class SignInTest {
 				new Date(1234567890), 1, 3,1,2, 1, false, true);
 		testResults.add(testMember);
 		model = new Model();
+		model.setLastLookupMemberResults(testResults);
+	}
+	
+	@Test
+	public void testSignIntoKitchen(){
+		System.out.println(testResults.size());
+		try{
+			ArrayList<Member> signInResults;// = new ArrayList<Member>();
+			signInResults = model.signIntoKitchen(0);
+			System.out.println(signInResults.size());
+			assertEquals(1, signInResults.size());
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
