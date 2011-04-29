ff47559fba10f4d0a6f9314fcaa92a746df63662
achin1 <chin.ashley@gmail.com>
2011-04-29 03:41:48 -0400

    sync Model with Database

--- src/Model.java
+++ src/Model.java
@@ -3,11 +3,36 @@ import java.util.ArrayList;
 
 public class Model 
 {
-	public int shiftLength;
 	private ArrayList<Member> signedIntoKitchen = new ArrayList<Member>();
 	private ArrayList<Member> signedIntoStore = new ArrayList<Member>();
-	
 	private Controller controllerReference; // Creates a reference to the controller.
+	private int shiftLength;
+	
+	/**
+	 * @author Ashley Chin
+	 * @version 4/27/11
+	 * 
+	 * Getter
+	 */
+			
+	public int getShiftLength()
+	{
+		return shiftLength;
+	}
+	
+	
+	/**
+	 * @author Ashley Chin
+	 * @version 4/27/11
+	 * 
+	 * Setter
+	 * @param shiftLengthIn
+	 */
+	public void setShiftLength(int shiftLengthIn)
+	{
+		this.shiftLength = shiftLengthIn;
+	}
+	
 	
 	/**
 	 * @author Ashley Chin
@@ -36,65 +61,8 @@ public class Model
 		return signedIntoStore;
 	}
 	
-	public ArrayList<Member> signIntoKitchen(int index) throws Exception
-	{
-		if(matches.get(index).canSignIn())
-		{
-			for(int i = 0; i < signedIntoKitchen.size(); i++)
-			{
-				if(signedIntoKitchen.get(i).equals(matches.get(index)))
-				{
-					throw new Exception("This member is already signed into the kitchen");
-				}
-			}
-			for(int i = 0; i < signedIntoStore.size(); i++)
-			{
-				if(signedIntoStore.get(i).equals(matches.get(index)))
-				{
-					throw new Exception("This member is already signed into the store");
-				}
-				long time;
-			matches.get(index).setLastSignIn(new Time(time));
-			signedIntoKitchen.add(matches.get(index));
-			}
-		}
-		else
-		{
-			throw new Exception("This member cannot be signed in");
-		}
-		
-		return signedIntoKitchen;
-	}
 	
-	public ArrayList<Member> signIntoStore(int index) throws Exception
-	{
-		if(matches.get(index).canSignIn())
-		{
-			for(int i = 0; i < signedIntoStore.size(); i++)
-			{
-				if(signedIntoStore.get(i).equals(matches.get(index)))
-				{
-					throw new Exception("This member is already signed into the kitchen");
-				}
-			}
-			for(int i = 0; i < signedIntoKitchen.size(); i++)
-			{
-				if(signedIntoKitchen.get(i).equals(matches.get(index)))
-				{
-					throw new Exception();
-				}
-				long time;
-			matches.get(index).setLastSignIn(new Time(time));
-			throw new Exception("This member is already signed into the store");
-			}
-		}
-		else
-		{
-			throw new Exception("This member cannot be signed in");
-		}
-		return signedIntoStore;
-	}
-
+	
 	/**
 	 * @author Ashley Chin
 	 * @version 4/14/11
@@ -111,66 +79,38 @@ public class Model
 
 	/**
 	 * @author Ashley Chin
-	 * @version 4/14/11
+	 * @version 4/28/11
 	 * 
 	 * Signs working member out of kitchen.
 	 * @param designated index of member in array list
 	 * @return modified array list of currently working members
 	 */
-	public ArrayList<Member> signOutOfKitchen(int index,int adjustedTime)
+	public ArrayList<Member> signOutOfKitchen(int index)
 	{
 		long startTime = signedIntoKitchen.get(index).getLastSignIn();
 		long stopTime = System.currentTimeMillis();
-		shiftLength = (int)(stopTime - startTime)*60000;
+		shiftLength = (int)(stopTime - startTime)/60000;
 	    int numberOfDiscounts;
        
-        if (adjustedTime==0)	// if ((shiftLength < 45) || (shiftLength > 120))
+        if ((shiftLength < 45) || (shiftLength > 120))
         {	
-           
-        	//int reconciledShiftLength = controllerReference.reconcileShiftLength(shiftLength);
-            //numberOfDiscounts = reconciledShiftLength/60;
-        //	DatabaseAbstraction.addDiscounts(signedIntoStore.get(index),numberOfDiscounts);
+          	shiftLength = controllerReference.reconcileShiftLength(shiftLength);
         }
-        
-        
-        	if (shiftLength>(1.5*60))
-            {
-            	numberOfDiscounts = 2; 
-            }
-            else
-            {
-            	numberOfDiscounts = 1;
-            }
-        
-        DatabaseAbstraction.addDiscounts(signedIntoStore.get(index),numberOfDiscounts);   
-    	/*----------------------------------------------------------------
-         * addDiscounts() incomplete.  MUST. FIX.
-         *
-         *-----------------------------------------------------------------
-         */
+        if (shiftLength >= 90)	
+        {
+        	numberOfDiscounts = 2; 
+        }
+        else
+        {
+        	numberOfDiscounts = 1;
+        }
+        signedIntoStore.get(index).setAvailableDiscounts(signedIntoStore.get(index).getAvailableDiscounts()+numberOfDiscounts);
+        DatabaseAbstraction.updateMember(signedIntoStore.get(index));   
         signedIntoKitchen.remove(index);
         return signedIntoKitchen;
     }
 	
-	
-   
-	/*
-	 * If so, the Reconcile Shift Length window will AUTOMATICALLY open, while locking 
-	 * the main window. Inside the Reconcile Shift Length window, there will be a textbox prompting 
-	 * the operator to input the new shift length (I still don't know if its going to be in minutes 
-	 * or hours). When the new shift length is inputted, it will percolate back to the model via the 
-	 * reconcileShiftLength() function. So yes, the int gets passed to the controller, then passes 
-	 * to the model, which will calculate the discounts. Unfortunately, I don't think the window pop-up
-	 *  for reconcile shift length has been created yet, or committed. However, I have attached the 4 
-	 *  new java files from the GUI that we received last class. You may find your answer there.
-
-
-
-	 */
-			
-	
-
-	
+	  
 	/**
 	 * @author Ashley Chin
 	 * @version 4/14/11
