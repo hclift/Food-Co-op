d6b2cc200b1b8c7d0d38497d009f87ce7fcee57f
Fahd Bouriss <fbouris1@binghamton.edu>
2011-05-02 00:44:14 -0400

    Added SignOutTest.java from Fahd Bouriss' email.

---
+++ src/SignOutTest.java
@@ -0,0 +1,51 @@
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+
+public class SignOutTest {
+	Controller TempController;
+	Model TempModel;
+	ArrayList<Member> TestStoreMemberList;
+	ArrayList<Member> TestKitchenMemberList;
+	ArrayList<Member> testResults;
+	Member testMember;
+	@Before
+	public void setUp() throws Exception {
+		TempModel = new Model();
+		testResults = new ArrayList<Member>();
+		testMember = new Member(13132, "John", "Doe", "jdoe@binghamton.edu", 
+				new Date(1234567890), 1, 1,1,2, 1, false, true);
+		testResults.add(testMember);
+		testMember = new Member(13902, "Raibi", "Jamila", "raibi1@binghamton.edu", 
+				new Date(1234567890), 1, 3,1,2, 1, false, true);
+		testResults.add(testMember);
+		TempModel.setLastLookupMemberResults(testResults);
+		TestStoreMemberList  = TempModel.signIntoStore(0);
+		TestKitchenMemberList = TempModel.signIntoKitchen(1);
+
+	}
+    @Test
+	public void testsignOutOfStore()
+    {
+		TestStoreMemberList.remove(0);
+		TempModel.signOutOfStore(0);
+		assertEquals(TestStoreMemberList,TempModel.getSignedIntoStore());
+	}
+    @Test
+	public void testsignOutOfKitchen()
+    {
+		TestKitchenMemberList.remove(0);
+		TempModel.signOutOfKitchen(0);
+		assertEquals(TestKitchenMemberList,TempModel.getSignedIntoKitchen());
+		
+	}
+}
+    
+
