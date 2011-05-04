0595653b3c2429e9270c6b10bc281e89b55a486f
Blake Schwartz <bschwar4@binghamton.edu>
2011-05-04 14:40:28 -0400

    Pushing SignInIntegrationTest by Christian

--- src/SignInIntegrationTest.java
+++ src/SignInIntegrationTest.java
@@ -13,44 +13,64 @@ public class SignInIntegrationTest {
 	ArrayList<Member>dummyArrayList = new ArrayList<Member>();
 	private int numSignedIntoKitchen;
 	private int numSignedIntoStore;
-	
+
 	@Before
 	public void setUp() throws Exception{
 		m = new Model();
 		c = new Controller(m);
 		mf = new MainFrame(c, m);
-		
-		//Sign in 2 members into the kitchen
-		dummyArrayList = c.lookUpMember("Vinny", "Chase");
-		m.signIntoKitchen(0);
-		dummyArrayList = c.lookUpMember("Eric", "Murphy");
-		m.signIntoKitchen(1);
-		numSignedIntoKitchen = dummyArrayList.size();
-		
-		//Sign in 2 members into the store
-		dummyArrayList = c.lookUpMember("Henry", "Hill");
-		m.signIntoStore(2);
-		dummyArrayList = c.lookUpMember("George", "Costanza");
-		m.signIntoStore(3);
-		numSignedIntoStore = dummyArrayList.size() - 2;	//minus two since the first two members in the arrayList are in the kitchen
-		
+
+		try{
+			//Sign in 2 members into the kitchen
+			dummyArrayList = c.lookUpMember("Vinny", "Chase");
+			m.signIntoKitchen(0);
+			dummyArrayList = c.lookUpMember("Eric", "Murphy");
+			m.signIntoKitchen(0);
+			numSignedIntoKitchen = dummyArrayList.size();
+		}
+		catch(Exception e){
+			System.out.println("Error in signing a member into the kitchen: " + e.getMessage());
+		}
+
+		try{
+			//Sign in 2 members into the store
+			dummyArrayList = c.lookUpMember("Henry", "Hill");
+			m.signIntoStore(0);
+			dummyArrayList = c.lookUpMember("George", "Costanza");
+			m.signIntoStore(0);
+			numSignedIntoStore = dummyArrayList.size() - 2;	//minus two since the first two members in the arrayList are in the kitchen
+		}
+		catch(Exception e){
+			System.out.println("Error in signing a member into the store: " + e.getMessage());
+		}
+
 	}
-	
+
 	@Test
 	public void testSignIntoKitchen{
-		//getSignedIntoKitchen returns an arrayList of members currently signed into kitchen
-		//The size of dummyArrayList in both methods should be 2 (number of members signed in)
-		//dummyArrayList will now contain members signed into kitchen
-		dummyArrayList = m.getSignedIntoKitchen();
-		assertEquals(numSignedIntoKitchen, dummyArrayList.size());
+		try{
+			//getSignedIntoKitchen returns an arrayList of members currently signed into kitchen
+			//The size of dummyArrayList in both methods should be 2 (number of members signed in)
+			//dummyArrayList will now contain members signed into kitchen
+			dummyArrayList = m.getSignedIntoKitchen();
+			assertEquals(numSignedIntoKitchen, dummyArrayList.size());
+		}
+		catch(Exception e){
+			System.out.println("Error in signIntoKitchen, size of array list is wrong: " + e.getMessage());
+		}
 	}
-	
+
 	@Test
 	public void testSignIntoStore{
-		//dummyArrayList will now contain members signed into store
-		//not completely sure this is accurate
-		dummyArrayList = m.getSignedIntoStore();
-		assertEquals(numSignedIntoStore, dummyArrayList.size());
+		try{
+			//dummyArrayList will now contain members signed into store
+			//not completely sure this is accurate
+			dummyArrayList = m.getSignedIntoStore();
+			assertEquals(numSignedIntoStore, dummyArrayList.size());
+		}
+		catch(Exception e){
+			System.out.println("Error in signIntoStore, size of array list is wrong: " + e.getMessage());
+		}
 	}
 
 }
