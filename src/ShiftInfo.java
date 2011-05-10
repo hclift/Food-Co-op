eadf6f0eb2d2b825d63577e0f80f160b847d9f48
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-05-10 11:06:45 -0400

    Add ShiftInfo class

--- src/ShiftInfo.java
+++ src/ShiftInfo.java
@@ -1,4 +1,30 @@
-
 public class ShiftInfo {
+	private int day;
+	private int month;
+	private int minWorked;
+	private int year;
+	
+	public ShiftInfo(int month, int day, int year, int minWorked) {
+		super();
+		this.day = day;
+		this.month = month;
+		this.year = year;
+		this.minWorked = minWorked;
+	}
 
+	public int getShiftDay() {
+		return day;
+	}
+
+	public int getShiftMonth() {
+		return month;
+	}
+
+	public int getMinWorked() {
+		return minWorked;
+	}
+
+	public int getShiftYear() {
+		return year;
+	}
 }
