c787c7c3ff6a4e184b6829485838fced83b19935
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-05-11 01:06:24 -0400

    Add member_id field to the shifts table
    IMPORTANT: This commit modifies the schema and requires that the
    database be recreated, see the README for instructions.

--- db/schema.sql
+++ db/schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE member_discounts (member_id INTEGER PRIMARY KEY AUTOINCREMENT, disc
 CREATE TABLE member_iou (member_id INTEGER PRIMARY KEY AUTOINCREMENT, iou_amount NUMERIC);
 CREATE TABLE members (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEXT, email_address TEXT, expiration_date NUMERIC, membership_type NUMERIC, year_in_school NUMERIC, is_active NUMERIC);
 CREATE TABLE schedule (id INTEGER PRIMARY KEY AUTOINCREMENT, day NUMERIC, hour NUMERIC, coordinator_id NUMERIC, number_of_volunteers NUMERIC);
-CREATE TABLE shifts (id INTEGER PRIMARY KEY AUTOINCREMENT, month NUMERIC, day NUMERIC, year NUMERIC, shift_length NUMERIC);
+CREATE TABLE shifts (id INTEGER PRIMARY KEY AUTOINCREMENT, member_id NUMERIC, month NUMERIC, day NUMERIC, year NUMERIC, shift_length NUMERIC);
 COMMIT;
